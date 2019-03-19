ebf141b198a09b639527666cba3937b36a85715c
Luis Santana <lsantana30@protonmail.com>
2019-03-18 18:04:14 -0700

    Added scoreboard and refactored. Fully functional.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,52 +1,63 @@
 
 /// Variables ///
-const board = {};
-let playerOne = true, win = false;
+const state = {
+    board = {},
+    scoreBoard = {'X': 0, 'O': 0},
+    playerOne = true, 
+    win = false, 
+    winner = null, 
+    first = 'X', 
+    second = 'O' 
+}; 
 
 /// Event Listener ///
-document.addEventListener('click', (event) => {
-    event.target.tagName === 'BUTTON' ? reset(true) : addPlay(event.target);
-});
+document.addEventListener('click', (event) => (event.target.classList[0] === 'restart' ? reset(true) : (event.target.classList[0] === 'submit') ? setPlayers() : (event.target.classList.length === 3) ? addPlay(event.target) : null));
 
 /// DOM Manipultation ///
 const addPlay = (target) => {
     let invalidPlay = false;
-    (target.textContent === "") ? (playerOne) ? target.textContent = 'X' : target.textContent = 'O' : invalidPlay = true;
+    (target.textContent === "") ? (playerOne) ? target.textContent = first : target.textContent = second : invalidPlay = true;
     if (!invalidPlay) checkForWin(target);
-    if (win || Object.keys(board).length === 9) endGame(); 
+    if (win || Object.keys(state.board).length === 9) endGame(); 
 }
 
 const reset = (bool) => {
     if (bool) {
+        first = (winner === null) ? 'X' : winner;
+        second = (winner === null) ? 'O' : (winner === 'X') ? 'O' : 'X'; 
         playerOne = true;
-        for (let key in board) delete board[key];
+        for (let key in state.board) delete state.board[key];
         const table = document.getElementsByClassName('tictac')[0].children;
-        for (let i = 0; i < table.length; i++) {
-            table[i].textContent = "";
-        }
+        for (let i = 0; i < table.length; i++) table[i].textContent = "";
     }
 }
 
+const updateScore = (winner) => (document.getElementsByClassName(winner)[0].textContent = ++scoreBoard[winner]);
+
+const setPlayers = () => (document.getElementsByClassName('players')[0].textContent = `X (${document.getElementsByClassName('names')[0].value}) vs O (${document.getElementsByClassName('names')[1].value})`);
+
 /// Checking for win ///
 const checkForWin = (target) => {
-    board[target.classList[2]] = (playerOne) ? 'X' : 'O';
+    state.board[target.classList[2]] = (playerOne) ? first : second;
     playerOne = !playerOne;
-    win = (Object.keys(board).length > 4) ? (checkRows(target.classList[0].split('row')[1]) || checkColumns(target.classList[1].split('column')[1]) || checkDiags()) : false;
+    win = (Object.keys(state.board).length > 4) ? (checkRows(target.classList[0].split('row')[1]) || checkColumns(target.classList[1].split('column')[1]) || checkDiags()) : false;
 }
 
-const checkRows = (row) => {
-    return (board['square' + row] === board['square' + (+row + 1)] && board['square' + (+row + 2)] === board['square' + row]);
-}
+const checkRows = (row) => (
+    (state.board['square' + row] === state.board['square' + (+row + 1)] && state.board['square' + (+row + 2)] === state.board['square' + row])
+);
 
-const checkColumns = (column) => {
-    return (board['square' + column] === board['square' + (+column + 1)] && board['square' + column] === board['square' + (+column + 2)]);
-}
+const checkColumns = (column) => (
+    (state.board['square' + column] === state.board['square' + (+column + 3)] && state.board['square' + column] === state.board['square' + (+column + 6)])
+);
 
-const checkDiags = () => {
-    return (board['square1'] === board['square5'] && board['square9'] === board['square5'] && board['square5'] !== "") || (board['square3'] === board['square5'] && board['square7'] === board['square5'] && board['square5'] !== "");
-}
+const checkDiags = () => (
+    (state.board['square1'] === state.board['square5'] && state.board['square9'] === state.board['square5'] && state.board['square5']) || (state.board['square3'] === state.board['square5'] && state.board['square7'] === state.board['square5'] && state.board['square5'])
+);
 
 const endGame = () => {
-    Object.keys(board).length === 9 && !win ? confirm("Tie game! \n Play new game?") ? reset(true) : reset(false) : confirm("Winner! \n Play new game?") ? reset(true) : reset(false);  
+    winner = (playerOne) ? second : first;
+    if (win) updateScore(winner);
+    Object.keys(state.board).length === 9 && !win ? confirm("Tie game! \nPlay new game?") ? reset(true) : reset(false) : confirm(`${winner} is the winner! \nPlay new game?`) ? reset(true) : reset(false);  
 }
 
