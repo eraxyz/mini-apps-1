0102efc127dc049aa718c671b2e87a280589001a
Luis Santana <lsantana30@protonmail.com>
2019-03-19 15:30:44 -0700

    Working with text field. Not with file picker yet.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+let result = "";
 
+app.use(express.static('client'))
+app.use(bodyParser.text());
 
+app.set('view engine', 'ejs');
+
+app.listen(port);
+
+app.post('/upload_json', function (req, res) {
+    let obj = JSON.parse(req.body.split('=')[1]);
+    result = Object.keys(obj).join(',');
+    
+    for (let prop in obj) if (typeof obj[prop] === 'object') result = result.split(',' + prop).join('');
+    
+    const repeat = (nextObj) => {
+        if (result.endsWith(',')) result = result.substring(0, result.length-2);
+        result += '\n';
+        for (let prop in nextObj) {
+            if (Array.isArray(nextObj[prop])) for (let child of nextObj[prop]) repeat(child);
+            else result += nextObj[prop].toString() + ',';
+        }
+    }
+    repeat(obj);
+    if (result.endsWith(',')) result = result.substring(0, result.length-2);
+    console.log(result);
+    res.send(result);
+});
